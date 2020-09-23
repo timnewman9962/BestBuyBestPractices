aa543cfe2257c79c5402aa19f390ddae2032a3f8
Tim Newman <timnewman9962@gmail.com>
2020-09-23 09:56:00 -0400

    Code format/legibility, fix GetProduct() usage

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.IO;
 using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace BestBuyBestPractices
 {
@@ -39,35 +40,35 @@ namespace BestBuyBestPractices
                         Console.WriteLine();
 
                         foreach (var item in repo.GetAllDepartments())
-                        {
                             Console.WriteLine(item.Name);
-                        }
 
                     } break;
-                    case "x":
-                        {
-                            var repo = new DapperDepartmentRepository(conn);
-                            foreach (var item in repo.GetAllDepartments())
-                                Console.WriteLine(item.Name);
-                          
-                            Console.WriteLine("\nEnter name of Department to remove:");
-                            string temp = Console.ReadLine();
-                            if(null == repo.GetDepartment(temp))
-                            {
-                                Console.WriteLine("Invalid Department Name");
-                                continue;
-                            }
 
-                            Console.WriteLine($"Deleting {temp} department\n");
-                            repo.DeleteDepartment(temp);
-                            foreach (var item in repo.GetAllDepartments())
-                                Console.WriteLine(item.Name);
+                    case "x":
+                    {
+                        var repo = new DapperDepartmentRepository(conn);
+                        foreach (var item in repo.GetAllDepartments())
+                            Console.WriteLine(item.Name);
+                          
+                        Console.WriteLine("\nEnter name of Department to remove:");
+                        string temp = Console.ReadLine();
+                        if(null == repo.GetDepartment(temp))
+                        {
+                            Console.WriteLine("Invalid Department Name");
+                            continue;
                         }
-                        break; // remove department
+
+                        Console.WriteLine($"Deleting {temp} department\n");
+                        repo.DeleteDepartment(temp);
+                        foreach (var item in repo.GetAllDepartments())
+                            Console.WriteLine(item.Name);
+                    } break;
+
                     case "q": quit = true; break;
+
                     case "p":
                     {
-                        Console.WriteLine("Add a new (Other) product:");
+                        Console.WriteLine("Add a new product (category = \'Other\'):");
                         var otherName = Console.ReadLine();
                         Console.WriteLine("Price:");
                         var otherPrice = Double.Parse(Console.ReadLine());
@@ -79,6 +80,7 @@ namespace BestBuyBestPractices
                         foreach (var item in prodRepo.GetAllProducts())
                             Console.WriteLine($"{item.Name}: ${item.Price}, ID {item.ProductID}");
                     } break;
+
                     case "u":
                     {
                         var prodRepo = new DapperProductRepository(conn);
@@ -120,6 +122,7 @@ namespace BestBuyBestPractices
                         queryProduct = prodRepo.GetProduct(changeProductID);
                         Console.WriteLine($"{queryProduct.Name}: ${queryProduct.Price}, ID {changeProductID}");
                     } break;
+
                     case "r":
                     {
                         var prodRepo = new DapperProductRepository(conn);
@@ -147,17 +150,27 @@ namespace BestBuyBestPractices
                         prodRepo.RemoveProduct(changeProductID);
 
                         var salesRepo = new DapperSalesRepository(conn);
-                        if (null != salesRepo.GetSalesItem(changeProductID))
+                        if (salesRepo.GetSalesItem(changeProductID).Count() != 0)
+                        {
+                            Console.WriteLine("Removing from Sales");
                             salesRepo.RemoveProduct(changeProductID);
+                        }
+                        else
+                            Console.WriteLine("Not found in Sales");
 
                         var reviewsRepo = new DapperReviewsRepository(conn);
-                        if (null != reviewsRepo.GetReviewItem(changeProductID))
+                        if (reviewsRepo.GetReviewItem(changeProductID).Count() != 0)
+                        {
+                            Console.WriteLine("Removing from Reviews");
                             reviewsRepo.RemoveProduct(changeProductID);
+                        }
+                        else
+                            Console.WriteLine("Not found in Reviews");
 
                         foreach (var item in prodRepo.GetAllProducts())
                             Console.WriteLine($"{item.Name}: ${item.Price}, ID {item.ProductID}");
-                        }
-                        break;
+                    } break;
+
                     default: quit = true; break;
                 }
             }
